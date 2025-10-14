28a6c8373127973bb3af2cd0004b680c06ae6777
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-14 04:17:45 -0400

    Added the registration endpoint

--- src/Components/RegisterPage.jsx
+++ src/Components/RegisterPage.jsx
@@ -22,7 +22,7 @@ const RegisterPage = () => {
     setError('');
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/auth/register/', {
+      const response = await fetch('https://mkulimadigital-backend.onrender.com/api/register/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

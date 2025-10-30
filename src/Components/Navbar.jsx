bd63ddd53804dfe0f06ec78be4995f40fdd21f8c
Henry-ai443 <henrymaina2024@outlook.com>
2025-10-30 06:31:13 -0400

    Remove email from the localstorage during logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,6 +14,7 @@ function Navbar() {
     localStorage.removeItem("token");
     localStorage.removeItem("role");
     localStorage.removeItem("username");
+    localStorage.removeItem('email');
     navigate("/");
   };
 
